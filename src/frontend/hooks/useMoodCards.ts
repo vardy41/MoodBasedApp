3c1a3048d74631a4608c5858a6a4d1b4d294b94b
Vardy41 <kokoko1325@gmail.com>
2025-07-28 20:20:44 +0200

    changed url

--- src/frontend/hooks/useMoodCards.ts
+++ src/frontend/hooks/useMoodCards.ts
@@ -5,14 +5,13 @@ export const useMoodCards = (playlistId: number) => {
 	const [data, setData] = useState<TrackProps[]>([]);
 	const [loading, setLoading] = useState(true);
 	const [error, setError] = useState<string | null>(null);
-
+	const backendUrl = import.meta.env.VITE_BACKEND_URL;
 	useEffect(() => {
 		const fetchData = async () => {
 			try {
 				setLoading(true);
-				const res = await fetch(
-					`http://localhost:3001/api/playlist/${playlistId}`
-				);
+
+				const res = await fetch(`${backendUrl}/api/playlist/${playlistId}`);
 
 				if (!res.ok) throw new Error("Błąd pobierania danych");
 				const json = await res.json();
